dd219a0eea26b63e730a7a8c3ddd77e4ef6cd1c7
stroppytux <gary@stroppytux.net>
2005-10-20 14:41:34 +0000

    Moved testxml.sh to compile.sh and added another window to test.xml
    git-svn-id: http://svn.stroppytux.net/xscroller/trunk@6 58d3bfee-5303-0410-8066-8f5428f14cd7

--- compile.sh
+++ compile.sh
@@ -1,14 +1,14 @@
 #!/bin/sh
 
 gcc -I /usr/include/libxml2 -I /usr/include/freetype2 -Wall -g -c -o libxscroller-static.o libxscroller.c
-echo "Stage 1 done"
+echo "3 Stages left"
 # Create static library.
 ar rcs libxscroller-static.a libxscroller-static.o
-echo "Stage 2 done"
+echo "2 Stages left"
 # Compile test program file.
 gcc -I /usr/include/libxml2 -I /usr/include/freetype2 -Wall -g -c testxml.c -o testxml.o
-echo "Stage 3 done"
+echo "1 Stage left"
 gcc -g -o testxml testxml.o  -I /usr/X11R6/include -I /usr/include/freetype2 -I /usr/include/X11/Xft -I /usr/include/libxml2 -L. -L/usr/X11/lib -L/usr/lib -lxscroller-static -lX11 -lXext -lXft -lpng -ljpeg -ltiff -lgif -lz -lm -lImlib -lxml2 -lpthread
-echo "Stage 4 done"
+echo "Completed compile"
 # Execute the program.
 ./testxml
